966e8aa27131fdc7490c95876083adc370459928
Micheal Evans <michealevans@gmail.com>
2026-04-25 17:34:26 -0400

    Fix scraper route position

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/leads', require('./routes/leads'));
+app.use('/api/scraper', require('./routes/scraper'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
